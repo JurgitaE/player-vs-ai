f83ad1162e8be3be899f6f9517514f78c57ca2bd
Jurgita <jurgita.eicinaite@yahoo.com>
2024-04-20 12:59:45 +0300

    game grid

--- javascript/Game.js
+++ javascript/Game.js
@@ -11,6 +11,10 @@ class Game {
         this.input = new InputHandler(this);
         this.player = new Player(this, 0, 0, 1, 0);
 
+        this.cellSize = 50;
+        this.columns = Math.floor(this.width / this.cellSize);
+        this.rows = Math.floor(this.height / this.cellSize);
+
         this.resize(window.innerWidth, window.innerHeight);
     }
 
@@ -21,10 +25,22 @@ class Game {
         this.height = this.canvas.height;
         this.ctx.fillStyle = 'blue';
 
+        this.columns = Math.floor(this.width / this.cellSize);
+        this.rows = Math.floor(this.height / this.cellSize);
+
         this.render();
     }
 
+    drawGrid() {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.columns; x++) {
+                this.ctx.strokeRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+            }
+        }
+    }
+
     render() {
+        this.drawGrid();
         // this.player.update();
         // this.player.draw();
     }
