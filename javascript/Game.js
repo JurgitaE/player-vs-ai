d5eb5a8dc192413b1d7bc2702315db55d9ba6604
Jurgita <jurgita.eicinaite@yahoo.com>
2024-04-22 15:04:30 +0300

    responsive grid

--- javascript/Game.js
+++ javascript/Game.js
@@ -5,22 +5,22 @@ class Game {
     constructor(canvas, context) {
         this.canvas = canvas;
         this.ctx = context;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
+        this.width;
+        this.height;
 
         this.input = new InputHandler(this);
         this.player = new Player(this, 0, 0, 1, 0);
 
         this.cellSize = 50;
-        this.columns = Math.floor(this.width / this.cellSize);
-        this.rows = Math.floor(this.height / this.cellSize);
+        this.columns;
+        this.rows;
 
         this.resize(window.innerWidth, window.innerHeight);
     }
 
     resize(width, height) {
-        this.canvas.width = Math.floor(width);
-        this.canvas.height = Math.floor(height);
+        this.canvas.width = width - (width % this.cellSize);
+        this.canvas.height = height - height / this.cellSize;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.ctx.fillStyle = 'blue';
